af2e3a4e51e47a8b20b63f9816b40fface1cf1d1
kronosoul <kronosoul>
2020-05-10 14:03:36 +1000

    package

--- _package.sh
+++ _package.sh
@@ -2,5 +2,7 @@
 glib-compile-schemas ./worksets@blipk.xyz/schemas
 #glib-compile-resources ./res/org.kronosoul.worksets.xml
 #mv ./res/org.kronosoul.worksets.gresource ./worksets@blipk.xyz
-zip -jr worksets@blipk.xyz.zip worksets@blipk.xyz
+cd worksets@blipk.xyz
+zip -r ../worksets@blipk.xyz.zip *
+cd ..
 zip worksets@blipk.xyz.zip install.sh 'Install Customised Workspaces.desktop'
