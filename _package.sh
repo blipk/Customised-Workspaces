1a928b35d00d6bfc3e274eef631606fdd775adb1
Kronosol <45532845+blipk@users.noreply.github.com>
2022-10-10 16:42:27 +1100

    Fix docs

--- _package.sh
+++ _package.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 rm -f ./worksets@blipk.xyz/schemas/gschemas.compiled
 glib-compile-schemas ./worksets@blipk.xyz/schemas
-#glib-compile-resources ./res/org.kronosoul.worksets.xml
-#mv ./res/org.kronosoul.worksets.gresource ./worksets@blipk.xyz
 rm -rf worksets@blipk.xyz.zip
 cd worksets@blipk.xyz
 zip -r ../worksets@blipk.xyz.zip *
